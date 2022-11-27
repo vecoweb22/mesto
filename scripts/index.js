20d1ec0647e36616eae1cc61bfaf572d00cb5184
vecoweb <vecoweb22@yandex.ru>
2022-11-27 23:26:30 +0300

    add css changes of popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,56 @@
-//texts
-const userNameText = document.querySelector(".profile__title"); //переменная касса имени профиля на странице
-const userJobText = document.querySelector(".profile__subtitle"); //переменная касса работы на странице
-//popup control
-const popupElement = document.querySelector(".popup"); //переменная всего popup
-const editProfile = document.querySelector(".profile__edit-button"); //переменная открыть редактирование профиля
-const closeButton = popupElement.querySelector(".popup__close-button"); //переменная закрыть редактирование
-const saveButton = document.querySelector(".popup__save-button"); //переменная кнопки сохранить
-//inputs
-let nameInput = document.querySelector("#popup__user-name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector("#popup__user-job"); // Воспользуйтесь инструментом .querySelector()
+const popupElement = document.querySelector('.popup'); //1 переменная всего popup
+const editProfile = document.querySelector('.profile__edit-button'); //1 переменная открыть редактирование профиля
+const closeButton = popupElement.querySelector('.popup__close-button'); //1 переменная закрыть редактирование
+const saveButton = document.querySelector('.popup__save-button'); //1 переменная кнопки сохранить
+const userName = document.querySelector('.profile__title'); //2 переменная касса имени профиля на странице
+const userJob = document.querySelector('.profile__subtitle'); //2 переменная касса работы на странице
 
-//init value
-let userName = userNameText.innerText;
-let userStatus = userJobText.innerText;
+let nameInput = document.querySelector('#popup__user-name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('#popup__user-job'); // Воспользуйтесь инструментом .querySelector()
 
-const writeUserDataToTexts = () => {
-  userNameText.innerText = userName;
-  userJobText.innerText = userStatus;
-};
 
-const writeUserDataToInputs = () => {
-  nameInput.value = userName;
-  jobInput.value = userStatus;
-};
+// const writeUserDataToInputs = () => {
+//   nameInput.value = userName;
+//   jobInput.value = userJob;
+// };
 
 function openPopup() {
   popupElement.style.display = 'flex'
-  writeUserDataToInputs();
+  // writeUserDataToInputs();
 }
 
 function closePopup() {
   popupElement.style.display = 'none'
 }
 
-editProfile.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
+editProfile.addEventListener('click', openPopup)
+closeButton.addEventListener('click', closePopup)
 
-window.addEventListener("load", () => {
-  writeUserDataToTexts();
-});
 
-nameInput.addEventListener("input", (e) => {
-  const value = e.target.value;
-  userName = value;
-});
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+nameInput.value = userName.textContent;
+jobInput.value = userJob.textContent;
 
-jobInput.addEventListener("input", (e) => {
-  const value = e.target.value;
-  userStatus = value;
-});
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
 
-saveButton.addEventListener("click", (e) => {
-  e.preventDefault();
+  // Находим поля формы в DOM
+
+  // Получите значение полей из свойства value
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+
   closePopup();
-  writeUserDataToTexts();
-});
+  // Выберите элементы, куда должны быть вставлены значения полей
+
+  // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
