897e7b07931d3a5c4ee0d14ebec066978e50bc5a
vecoweb <vecoweb22@yandex.ru>
2022-12-22 21:44:03 +0300

    Add files project

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const galleryList = [
+  {
+    title: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    title: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    title: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    title: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    title: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    title: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,113 @@
-const popupElement = document.querySelector('.popup');
-const profileEdit = document.querySelector('.profile__edit-button');
-const closeButton = popupElement.querySelector('.popup__close-button');
-const userName = document.querySelector('.profile__title');
-const userJob = document.querySelector('.profile__subtitle');
+const main = document.querySelector('.main')
+const editButton = main.querySelector('.profile__edit-button');
+const addButton = main.querySelector('.profile__add-button');
+const popupEdit = document.querySelector('#popup-user');
+const popupAdd = document.querySelector('#popup-place');
+const popupImage = document.querySelector('.popup_opened-img');
+const closeButtons = document.querySelectorAll('.button_action_close');
+const editForm = document.querySelector('[name="edit-form"]');
+const addForm = document.querySelector('[name="add-form"]');
+const username = main.querySelector('.profile__title');
+const userjob = main.querySelector('.profile__subtitle');
+const inputUsername = document.querySelector('[name="user-name"]');
+const inputUserjob = document.querySelector('[name="user-job"]');
+const inputImgtitle = document.querySelector('[name="place-title"]');
+const iinputImgLink = document.querySelector('[name="photo-link"]');
+const gallery = main.querySelector('.gallery__list');
+const galleryTemplate = document.querySelector('#gallery-item').content.querySelector('.card');
+const fullImage = document.querySelector('.full-screen__image');
+const fullImageCaption = document.querySelector('.full-screen__caption');
 
-const nameInput = document.querySelector('#popup__user-name');
-const jobInput = document.querySelector('#popup__user-job');
 
-function openPopup() {
-  popupElement.classList.add('popup_opened');
-  nameInput.value = userName.textContent;
-  jobInput.value = userJob.textContent;
+const openPopup = (popup) => {
+  popup.classList.add('popup_opened');
 }
 
-function closePopup() {
-  popupElement.classList.remove('popup_opened');
+const closePopup = (popup) => {
+  popup.classList.remove('popup_opened');
 }
 
-let formElement = document.querySelector('.popup__form');
+const createElement = (item) => {
+  const galleryItem = galleryTemplate.cloneNode(true);
+  console.log(galleryItem);
+  const galleryItemTitle = galleryItem.querySelector('.card__title');
+  const galleryItemImg = galleryItem.querySelector('.card__image');
+  const likeButton = galleryItem.querySelector('.card__like');
+  const deleteButton = galleryItem.querySelector('.card__delete');
 
-function formSubmitHandler(evt) {
+  galleryItemImg.addEventListener('click', function () {
+    openPopup(popupImage)
+
+    fullImage.src = item.link;
+    fullImage.alt = galleryItemImg.alt
+    fullImageCaption.textContent = item.title;
+  });
+
+  likeButton.addEventListener('click', handleLikeButton);
+  deleteButton.addEventListener('click', handleDeleteButton);
+
+  galleryItemImg.src = item.link;
+  galleryItemImg.alt = `${item.title}. Автор: ${username.textContent}`
+  galleryItemTitle.textContent = item.title;
+
+  return galleryItem;
+}
+
+const renderItem = (item) => {
+  const element = createElement(item);
+  gallery.prepend(element);
+}
+
+
+const formSubmitHandlerEdit = (evt) => {
   evt.preventDefault();
-  userName.textContent = nameInput.value;
-  userJob.textContent = jobInput.value;
 
-  closePopup();
+  username.textContent = inputUsername.value;
+  userjob.textContent = inputUserjob.value;
+
+  closePopup(popupEdit);
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-profileEdit.addEventListener('click', openPopup)
-closeButton.addEventListener('click', closePopup)
+const formSubmitHandlerAdd = (evt) => {
+  evt.preventDefault();
+
+  const galleryItem = {
+    title: inputImgtitle.value,
+    link: iinputImgLink.value,
+  }
+
+  renderItem(galleryItem);
+
+  addForm.reset()
+  closePopup(popupAdd);
+}
+
+const handleLikeButton = (evt) => {
+  evt.target.classList.toggle('card__like_active')
+};
+
+const handleDeleteButton = (evt) => {
+  evt.target.closest('.card').remove()
+};
+
+galleryList.forEach(renderItem);
+
+addButton.addEventListener('click', () => openPopup(popupAdd));
+
+editButton.addEventListener('click', () => {
+  openPopup(popupEdit);
+
+  inputUsername.value = username.textContent;
+  inputUserjob.value = userjob.textContent;
+});
+
+editForm.addEventListener('submit', formSubmitHandlerEdit);
+
+addForm.addEventListener('submit', formSubmitHandlerAdd);
+
+closeButtons.forEach(function (item) {
+  const close = item.closest('.popup');
+  item.addEventListener('click', () => closePopup(close))
+});
+
+fullImage.addEventListener('click', () => closePopup(popupImage));
